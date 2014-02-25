e97b37bdc2e8a155a16f1db4912304ae3cc60ad6
josahlmann <josahlmann@users.noreply.github.com>
2014-02-25 09:19:49 +0100

    another test commit with more files

--- test1.py
+++ test1.py
@@ -2,6 +2,6 @@ import numpy as np
 
 print "*********************"
 # pl.close('all');
-srcDir = "/Users/sahlmann/astro/projects/bootcamp2014/code/";    
+srcDir = "/Users/sahlmann/astro/projects/bootcamp2014/";    
 
 
---
+++ test2.py
@@ -0,0 +1,12 @@
+import numpy as np
+import sys
+
+print "*********************"
+# pl.close('all');
+srcDir = "/Users/sahlmann/astro/projects/bootcamp2014/";
+
+sys.exit();
+
+    
+
+
